a6e4cefa4c0949cfd5396cab60892af993b97c6d
Minh Tri <MinhTri12995@users.noreply.github.com>
2026-07-07 22:23:34 +0700

    Add 10 more proxies from new account zrxvjoin

--- proxies_list.py
+++ proxies_list.py
@@ -12,7 +12,19 @@ ROTATING_PROXIES = [
     "http://xktamdun:985qtdjr7nnw@2.57.21.2:7239/",
     "http://xktamdun:985qtdjr7nnw@38.154.185.97:6370/",
     "http://xktamdun:985qtdjr7nnw@142.111.67.146:5611/",
-    "http://xktamdun:985qtdjr7nnw@191.96.254.138:6185/"
+    "http://xktamdun:985qtdjr7nnw@191.96.254.138:6185/",
+    
+    # Tài khoản mới thêm (10 proxy)
+    "http://zrxvjoin:5vtqaj0i1897@31.59.20.176:6754/",
+    "http://zrxvjoin:5vtqaj0i1897@31.56.127.193:7684/",
+    "http://zrxvjoin:5vtqaj0i1897@45.38.107.97:6014/",
+    "http://zrxvjoin:5vtqaj0i1897@198.105.121.200:6462/",
+    "http://zrxvjoin:5vtqaj0i1897@64.137.96.74:6641/",
+    "http://zrxvjoin:5vtqaj0i1897@198.23.243.226:6361/",
+    "http://zrxvjoin:5vtqaj0i1897@2.57.21.2:7239/",
+    "http://zrxvjoin:5vtqaj0i1897@38.154.185.97:6370/",
+    "http://zrxvjoin:5vtqaj0i1897@142.111.67.146:5611/",
+    "http://zrxvjoin:5vtqaj0i1897@191.96.254.138:6185/"
 ]
 
 def get_random_proxy():
