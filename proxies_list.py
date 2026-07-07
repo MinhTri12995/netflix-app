1a2827f27ba3c103d776d5e19b9999ec91c12937
Minh Tri <MinhTri12995@users.noreply.github.com>
2026-07-07 22:22:00 +0700

    Update proxy list with new Webshare account credentials

--- proxies_list.py
+++ proxies_list.py
@@ -3,16 +3,16 @@ import random
 # Danh sách các tài khoản Webshare (Rotating Proxy)
 # Code sẽ bốc ngẫu nhiên 1 tài khoản để sử dụng cho mỗi lượt khách lấy Link
 ROTATING_PROXIES = [
-    "http://nodzxvcx:tton2hgnp5so@31.59.20.176:6754/",
-    "http://nodzxvcx:tton2hgnp5so@31.56.127.193:7684/",
-    "http://nodzxvcx:tton2hgnp5so@45.38.107.97:6014/",
-    "http://nodzxvcx:tton2hgnp5so@198.105.121.200:6462/",
-    "http://nodzxvcx:tton2hgnp5so@64.137.96.74:6641/",
-    "http://nodzxvcx:tton2hgnp5so@198.23.243.226:6361/",
-    "http://nodzxvcx:tton2hgnp5so@2.57.21.2:7239/",
-    "http://nodzxvcx:tton2hgnp5so@38.154.185.97:6370/",
-    "http://nodzxvcx:tton2hgnp5so@142.111.67.146:5611/",
-    "http://nodzxvcx:tton2hgnp5so@191.96.254.138:6185/"
+    "http://xktamdun:985qtdjr7nnw@31.59.20.176:6754/",
+    "http://xktamdun:985qtdjr7nnw@31.56.127.193:7684/",
+    "http://xktamdun:985qtdjr7nnw@45.38.107.97:6014/",
+    "http://xktamdun:985qtdjr7nnw@198.105.121.200:6462/",
+    "http://xktamdun:985qtdjr7nnw@64.137.96.74:6641/",
+    "http://xktamdun:985qtdjr7nnw@198.23.243.226:6361/",
+    "http://xktamdun:985qtdjr7nnw@2.57.21.2:7239/",
+    "http://xktamdun:985qtdjr7nnw@38.154.185.97:6370/",
+    "http://xktamdun:985qtdjr7nnw@142.111.67.146:5611/",
+    "http://xktamdun:985qtdjr7nnw@191.96.254.138:6185/"
 ]
 
 def get_random_proxy():
