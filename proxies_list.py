de13b5937214d697cdae6b95787677a0bcf51ec8
Minh Tri <MinhTri12995@users.noreply.github.com>
2026-07-12 02:15:37 +0700

    Update proxies list with the new Webshare proxies

--- proxies_list.py
+++ proxies_list.py
@@ -3,28 +3,29 @@ import random
 # Danh sách các tài khoản Webshare (Rotating Proxy)
 # Code sẽ bốc ngẫu nhiên 1 tài khoản để sử dụng cho mỗi lượt khách lấy Link
 ROTATING_PROXIES = [
-    "http://xktamdun:985qtdjr7nnw@31.59.20.176:6754/",
-    "http://xktamdun:985qtdjr7nnw@31.56.127.193:7684/",
-    "http://xktamdun:985qtdjr7nnw@45.38.107.97:6014/",
-    "http://xktamdun:985qtdjr7nnw@198.105.121.200:6462/",
-    "http://xktamdun:985qtdjr7nnw@64.137.96.74:6641/",
-    "http://xktamdun:985qtdjr7nnw@198.23.243.226:6361/",
+    # Nhóm 1: fgdhvjbc
+    "http://fgdhvjbc:husxe5amjib9@31.59.20.176:6754/",
+    "http://fgdhvjbc:husxe5amjib9@31.56.127.193:7684/",
+    "http://fgdhvjbc:husxe5amjib9@45.38.107.97:6014/",
+    "http://fgdhvjbc:husxe5amjib9@198.105.121.200:6462/",
+    "http://fgdhvjbc:husxe5amjib9@64.137.96.74:6641/",
+    "http://fgdhvjbc:husxe5amjib9@198.23.243.226:6361/",
+    "http://fgdhvjbc:husxe5amjib9@38.154.185.97:6370/",
+    "http://fgdhvjbc:husxe5amjib9@84.247.60.125:6095/",
+    "http://fgdhvjbc:husxe5amjib9@142.111.67.146:5611/",
+    "http://fgdhvjbc:husxe5amjib9@191.96.254.138:6185/",
 
-    "http://xktamdun:985qtdjr7nnw@38.154.185.97:6370/",
-    "http://xktamdun:985qtdjr7nnw@142.111.67.146:5611/",
-    "http://xktamdun:985qtdjr7nnw@191.96.254.138:6185/",
-    
-    # Tài khoản mới thêm (10 proxy)
-    "http://zrxvjoin:5vtqaj0i1897@31.59.20.176:6754/",
-    "http://zrxvjoin:5vtqaj0i1897@31.56.127.193:7684/",
-    "http://zrxvjoin:5vtqaj0i1897@45.38.107.97:6014/",
-    "http://zrxvjoin:5vtqaj0i1897@198.105.121.200:6462/",
-    "http://zrxvjoin:5vtqaj0i1897@64.137.96.74:6641/",
-    "http://zrxvjoin:5vtqaj0i1897@198.23.243.226:6361/",
-
-    "http://zrxvjoin:5vtqaj0i1897@38.154.185.97:6370/",
-    "http://zrxvjoin:5vtqaj0i1897@142.111.67.146:5611/",
-    "http://zrxvjoin:5vtqaj0i1897@191.96.254.138:6185/"
+    # Nhóm 2: gvxrfmsr
+    "http://gvxrfmsr:cfd5ym3vkh1h@31.59.20.176:6754/",
+    "http://gvxrfmsr:cfd5ym3vkh1h@31.56.127.193:7684/",
+    "http://gvxrfmsr:cfd5ym3vkh1h@45.38.107.97:6014/",
+    "http://gvxrfmsr:cfd5ym3vkh1h@198.105.121.200:6462/",
+    "http://gvxrfmsr:cfd5ym3vkh1h@64.137.96.74:6641/",
+    "http://gvxrfmsr:cfd5ym3vkh1h@198.23.243.226:6361/",
+    "http://gvxrfmsr:cfd5ym3vkh1h@38.154.185.97:6370/",
+    "http://gvxrfmsr:cfd5ym3vkh1h@84.247.60.125:6095/",
+    "http://gvxrfmsr:cfd5ym3vkh1h@142.111.67.146:5611/",
+    "http://gvxrfmsr:cfd5ym3vkh1h@191.96.254.138:6185/"
 ]
 
 def get_random_proxy():
