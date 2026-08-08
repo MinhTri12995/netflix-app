44e781c4cf46d8a74aeec2d8c2421ba43baa1a39
Minh Tri <MinhTri12995@users.noreply.github.com>
2026-08-08 13:11:42 +0700

    feat: switch proxy provider to self-hosted AWS EC2 Free Tier proxy (18.143.199.199:3128)

--- proxies_list.py
+++ proxies_list.py
@@ -1,9 +1,9 @@
 import random
 
-# Webshare Single Backbone Rotating Proxy Endpoint
-SINGLE_ROTATING_PROXY = "http://nodzxvcx:tton2hgnp5so@31.59.20.176:6754/"
+# AWS EC2 Free Tier Proxy Server
+SINGLE_ROTATING_PROXY = "http://admin:Proxy123456@18.143.199.199:3128"
 
-USE_DIRECT_RENDER = False  # Bật False để dùng Proxy xoay vòng Webshare mới
+USE_DIRECT_RENDER = False  # Bật False để dùng Proxy AWS
 
 def get_random_proxy():
     if USE_DIRECT_RENDER:
